c7b75ae3958ae16f78b07a606b4ffdbd0e016d8b
Chuwei Xu <64442606+Chu-Wx@users.noreply.github.com>
2021-08-12 12:46:45 -0400

    Create 62.py

---
+++ File/62.py
@@ -0,0 +1,20 @@
+def uniquePaths(self, m: int, n: int) -> int:
+        # O(m*n) space   
+
+        if not m or not n:
+            return 0
+        dp = [[1 for _ in range(n)] for _ in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
+      
+def uniquePaths(self, m: int, n: int) -> int:
+    # O(n) space 
+    if not m or not n:
+        return 0
+    cur = [1] * n
+    for i in xrange(1, m):
+        for j in xrange(1, n):
+            cur[j] += cur[j-1]
+    return cur[-1]
