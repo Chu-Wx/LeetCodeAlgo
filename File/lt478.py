b4a73dee9943b0ac12b172a145d129e36ba8cddc
Chuwei Xu <64442606+Chu-Wx@users.noreply.github.com>
2021-09-10 17:58:39 -0400

    Create lt478.py

---
+++ File/lt478.py
@@ -0,0 +1,10 @@
+def calculate(self, a, operator, b):
+        if operator =="+":
+            return a+b
+        if operator=="-":
+            return a-b
+
+        if operator=="*":
+            return a*b
+        if operator=="/":
+            return a//b
